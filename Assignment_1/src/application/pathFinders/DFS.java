5ca7f6b87c700f2baf1f0dcb54ac6dc846daba13
super <jamaleldin.ahmed98@gmail.com>
2020-11-15 22:27:42 +0200

    Depth, time and cost of DFS

--- Assignment_1/src/application/pathFinders/DFS.java
+++ Assignment_1/src/application/pathFinders/DFS.java
@@ -9,30 +9,41 @@ import application.pathFinders.utilities.Node;
 import application.pathFinders.utilities.Utilities;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class DFS implements IPathFinder {
 	private ArrayList<int[]> visitedStates;
 	private Stack<INode> frontier;
 	private IUtilities utilities;
-
+	private int depth;
+	private long time;
+	private int cost;
 
 	DFS() {
 		visitedStates = new ArrayList<int[]>();
 		frontier = new Stack<INode>();
 		utilities = new Utilities();
+		depth = 0;
+		cost = 0;
 	}
 
 	@Override
 	public ArrayList<int[]> findPath(int[] initialState, String options) {
+		time = new Date().getTime();
 		visitedStates.clear();
 		frontier.clear();
 		frontier.push(new Node(initialState));
 		while (!frontier.isEmpty()) {
 			INode state = frontier.pop();
+			depth =  ((Node) state).getDepth();
 			visitedStates.add(state.getItem());
 
-			if (utilities.goalTest(state.getItem()))
-				return utilities.getPath(state);
+			if (utilities.goalTest(state.getItem())) {
+				time = new Date().getTime() - time;
+				ArrayList<int[]> path = utilities.getPath(state);
+				cost = path.size();
+				return path;
+			}
 
 			ArrayList<int[]> neighbors = utilities.getNeighbors(state.getItem());
 
@@ -41,7 +52,7 @@ public class DFS implements IPathFinder {
 				boolean inVisited = utilities.checkStateVisited(neighbor, visitedStates);
 
 				if (!inFrontier && !inVisited) {
-					INode nextNeighbor = new Node(neighbor);
+					INode nextNeighbor = new Node(neighbor, depth + 1);
 					nextNeighbor.setPrev(state);
 					frontier.push(nextNeighbor);
 				}
@@ -52,20 +63,17 @@ public class DFS implements IPathFinder {
 
 	@Override
 	public int CostOfPath() {
-		// TODO Auto-generated method stub
-		return 0;
+		return cost;
 	}
 
 	@Override
 	public int timeColapsed() {
-		// TODO Auto-generated method stub
-		return 0;
+		return (int) time;
 	}
 
 	@Override
 	public int depthOfsearch() {
-		// TODO Auto-generated method stub
-		return 0;
+		return depth;
 	}
 
 	@Override
